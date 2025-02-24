b047f11920d0cc0b26bc087c6d702d585665b846
Devansh Singh <161079420+DevanshSingh775@users.noreply.github.com>
2025-02-24 16:38:17 +0530

    Add files via upload

---
+++ Devansh Append.py
@@ -0,0 +1,20 @@
+'''v=int(input("enter a data to append:"))
+a.append(v)
+b=int(input("enter a data to append:"))
+a.append(b)
+c=int(input("enter a data to append:"))
+a.append(c)
+n=int(input("enter a data to append:"))
+a.append(n)
+d=int(input("enter a data to append:"))
+a.append(d)
+print(a)'''
+x=int(input("ENTER THE NO. OF ELEMENTS :"))
+a=[]
+i=0
+while(i<x):
+    b=int(input("enter the number;"))
+    a.append(b)
+    i=i+1
+print(a)   
+    
