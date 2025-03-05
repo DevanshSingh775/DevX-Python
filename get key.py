c7c0e4c9b4b7fe12ee77eac21c563032fadd1ef0
Devansh Singh <161079420+DevanshSingh775@users.noreply.github.com>
2025-03-05 23:28:19 +0530

    Add files via upload

---
+++ get key.py
@@ -0,0 +1,10 @@
+d1={}
+i=0
+n=int(input("enter the num of items:"))
+while(i<n):
+    key=input("enter the value of keys:")
+    data=input("enter the value of data:")
+    i+=1
+    d1[key]=data
+print(d1)
+print(d1.get((input("key:")),'key not exist'))
